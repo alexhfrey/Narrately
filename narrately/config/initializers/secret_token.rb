bd262d7ca62533137467b1fbc30644f445bb445c
alexhfrey <alex.h.frey@gmail.com>
2012-01-12 11:00:20 -0800

    Initial Commit

---
+++ narrately/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Narrately::Application.config.secret_token = 'c8ccd24758e919a4cabbc5e41e11489ed517e09eb0fc1708dd6b8854467983310b85bf78fd0e7571f61bc41e523158befc6ab26f643f3f6a9dc9056523dcb342'
